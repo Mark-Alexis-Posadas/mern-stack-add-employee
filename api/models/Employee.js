b4977c14b07b127eadf26da26207edc6c35b2d98
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-08-20 18:34:56 +0800

    add endpoint and schema

---
+++ api/models/Employee.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const addEmployeeSchema = new Schema({
+  Id: {
+    type: Number,
+    required,
+  },
+  firstName: {
+    type: String,
+    required,
+  },
+  middleName: {
+    type: String,
+    required,
+  },
+  lastName: {
+    type: String,
+    required,
+  },
+
+  email: {
+    type: String,
+    required,
+  },
+});
+
+module.exports = mongoose.model("Employee", addEmployeeSchema);
--- api/server.js
+++ api/server.js
@@ -3,8 +3,29 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
+//GET all employess
 app.get("/", (req, res) => {
   res.send({ message: "hello world" });
+});
+
+//POST new employee
+app.post("/api/employees", (req, res) => {
+  res.send({ message: "create new employee" });
+});
+
+//GET single employee
+app.get("/api/employess:id", (req, res) => {
+  res.send({ message: "get single employee" });
+});
+
+//DELETE single employee
+app.delete("/api/employess:id", (req, res) => {
+  res.send({ message: "delete employee" });
+});
+
+//UPDATE single employee
+app.patch("/api/employess:id", (req, res) => {
+  res.send({ message: "update employee" });
 });
 
 app.listen(PORT, () => {
