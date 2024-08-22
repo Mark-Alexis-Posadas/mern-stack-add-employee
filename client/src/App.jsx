33602f5dad57b65902a73fc8bd481111bb6fdb0a
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-08-22 12:08:34 +0800

    added layouts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import AddEmployee from "./pages/AddEmployee";
+import AuthenticationLayout from "./layouts/AuthenticationLayout";
+import EmployeeLayout from "./layouts/EmployeeLayout";
+import Login from "./pages/Login";
 
 const App = () => {
-  return <AddEmployee />;
+  return (
+    <Routes>
+      <Route element={<AuthenticationLayout />}>
+        <Route path="/login" element={<Login />} />
+      </Route>
+
+      <Route path="/" element={<EmployeeLayout />}>
+        <Route index element={<AddEmployee />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
---
+++ client/src/layouts/AuthenticationLayout.jsx
@@ -0,0 +1,8 @@
+import { Outlet } from "react-router-dom";
+export default function AuthenticationLayout() {
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+}
