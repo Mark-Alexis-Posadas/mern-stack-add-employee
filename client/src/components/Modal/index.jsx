c41e56dd28e6873230fb5b4e801d901d6a18d3f2
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-08-28 11:02:46 +0800

    added toggle theme and google fonts

--- client/src/components/Modal/index.jsx
+++ client/src/components/Modal/index.jsx
@@ -1,6 +1,6 @@
 import { faCircleXmark, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-export default function Modal() {
+export default function Modal({ handleCancelModal }) {
   return (
     <div className="flex items-center justify-center fixed w-full top-0 left-0 min-h-screen bg-[rgba(0,0,0,0.4)]">
       <form className="bg-white dark:bg-gray-700 p-5 md:p-10 w-[900px]">
@@ -8,7 +8,7 @@ export default function Modal() {
           Add fields
         </h1>
 
-        <p className="text-red-600 text-xl mb-5">Already exist!</p>
+        {/* <p className="text-red-600 text-xl mb-5">Already exist!</p> */}
 
         <div className="relative z-0 w-full mb-2 md:mb-5 group">
           <label className="text-gray-400 text-sm" htmlFor="first_name">
@@ -58,7 +58,10 @@ export default function Modal() {
         </div>
 
         <div className="flex items-center gap-3">
-          <button className="text-white rounded p-2 bg-red-600 flex items-center gap-2">
+          <button
+            className="text-white rounded p-2 bg-red-600 flex items-center gap-2"
+            onClick={handleCancelModal}
+          >
             cancel
             <FontAwesomeIcon icon={faCircleXmark} />
           </button>
--- client/src/pages/AddEmployee.jsx
+++ client/src/pages/AddEmployee.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon, faPlusCircle, faSun } from "@fortawesome/free-solid-svg-icons";
 
@@ -6,18 +7,32 @@ import Modal from "../components/Modal";
 import ConfirmationModal from "../components/Modal/ConfirmModal";
 
 export default function AddEmployee() {
+  const [isToggleModal, setIsToggleModal] = useState(false);
+  const [isToggleTheme, setIstoggleTheme] = useState(false);
+
+  const handleToggleTheme = () => {
+    setIstoggleTheme(!isToggleTheme);
+    document.body.classList.toggle("dark");
+  };
+  const handleCancelModal = () => {
+    setIsToggleModal(false);
+  };
+
   return (
     <div className="p-5 md:p-10 bg-slate-50 dark:bg-black min-h-screen overflow-hidden">
       <div className="flex items-center justify-between">
         <button
           className="dark:text-gray-400 p-2 rounded bg-blue-600 text-white dark:bg-gray-800 mb-5 flex items-center gap-3"
-          onClick={() => dispatch({ type: "HANDLE_ADD_EMPLOYEE" })}
+          onClick={() => setIsToggleModal(true)}
         >
           Add employee
           <FontAwesomeIcon icon={faPlusCircle} />
         </button>
-        <button>
-          <FontAwesomeIcon className="text-yellow-400 text-2xl cursor-pointer" />
+        <button onClick={handleToggleTheme}>
+          <FontAwesomeIcon
+            className="text-yellow-400 text-2xl cursor-pointer"
+            icon={isToggleTheme ? faSun : faMoon}
+          />
         </button>
       </div>
       <div className="relative overflow-x-auto shadow-md">
@@ -31,8 +46,9 @@ export default function AddEmployee() {
         </table>
       </div>
 
-      {/* <Modal />
-      <ConfirmationModal /> */}
+      {isToggleModal && <Modal handleCancelModal={handleCancelModal} />}
+
+      {/* <ConfirmationModal /> */}
     </div>
   );
 }
