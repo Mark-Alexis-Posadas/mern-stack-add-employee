ae5b7239b31f8c33525f7f659b00bd33112f7535
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-11-07 07:42:49 +0800

    done optimizing sign up form

--- client/src/pages/SignUp/index.jsx
+++ client/src/pages/SignUp/index.jsx
@@ -1,9 +1,42 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import * as Yup from "yup";
 import { ErrorMessage, Field, Formik } from "formik";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import Eye from "../../components/Eye";
 import { Input } from "../../components/Input/Input";
+import { Error } from "../../components/Error/Error";
+
+const Schema = Yup.object().shape({
+  firstName: Yup.string()
+    .matches(/^[A-Za-z]+$/, "First name can only contain letters")
+    .min(2, "First name must be at least 2 characters")
+    .max(50, "First name can be at most 50 characters")
+    .required("First name is required"),
+
+  middleName: Yup.string()
+    .matches(/^[A-Za-z]*$/, "Middle name can only contain letters")
+    .min(2, "Middle name must be at least 2 characters")
+    .max(50, "Middle name can be at most 50 characters")
+    .notRequired(), // Middle name is optional
+
+  lastName: Yup.string()
+    .matches(/^[A-Za-z]+$/, "Last name can only contain letters")
+    .min(2, "Last name must be at least 2 characters")
+    .max(50, "Last name can be at most 50 characters")
+    .required("Last name is required"),
+
+  email: Yup.string().email("Invalid email").required("Required"),
+
+  password: Yup.string()
+    .min(8, "Password is too short - should be 8 chars minimum.")
+    .matches(/[a-zA-Z]/, "Password can only contain Latin letters.")
+    .required("Password is required"),
+
+  confirmPassword: Yup.string()
+    .oneOf([Yup.ref("password"), null], "Passwords must match") // Ensure it matches the password
+    .required("Confirm Password is required"),
+});
 
 export default function SignUp() {
   const [isPasswordVisible, setPasswordVisibility] = useState(false);
@@ -29,7 +62,7 @@ export default function SignUp() {
         password: "",
         confirmPassword: "",
       }}
-      validate={(values) => {}}
+      validationSchema={Schema}
       onSubmit={async (values, { setSubmitting }) => {
         try {
           await axios.post("http://localhost:4000/auth/sign-up", values);
@@ -56,7 +89,7 @@ export default function SignUp() {
           onSubmit={handleSubmit}
         >
           <h1 className="text-3xl font-bold mb-6 text-center">Sign Up</h1>
-
+          <ErrorMessage component={Error} name="firstName" />
           <Field
             label="First Name"
             type="text"
@@ -71,8 +104,8 @@ export default function SignUp() {
               errors.firstName ? "border-red-500" : "border-gray-300 "
             } mt-1 block w-full p-2 border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
           />
-          <ErrorMessage name="firstName" />
 
+          <ErrorMessage component={Error} name="middleName" />
           <Field
             label="Middle Name"
             type="text"
@@ -87,8 +120,7 @@ export default function SignUp() {
               errors.middleName ? "border-red-500" : "border-gray-300 "
             } mt-1 block w-full p-2 border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
           />
-          <ErrorMessage name="middleName" />
-
+          <ErrorMessage component={Error} name="lastName" />
           <Field
             label="Last Name"
             type="text"
@@ -103,8 +135,7 @@ export default function SignUp() {
               errors.lastName ? "border-red-500" : "border-gray-300 "
             } mt-1 block w-full p-2 border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
           />
-          <ErrorMessage name="lastName" />
-
+          <ErrorMessage component={Error} name="email" />
           <Field
             label="Email"
             type="email"
@@ -119,7 +150,6 @@ export default function SignUp() {
               errors.email ? "border-red-500" : "border-gray-300 "
             } mt-1 block w-full p-2 border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
           />
-          <ErrorMessage name="email" />
 
           <div className="mb-4 relative">
             {values.password.length > 0 && (
@@ -131,7 +161,7 @@ export default function SignUp() {
 
             <Field
               label="Password"
-              type="password"
+              type={isPasswordVisible ? "text" : "password"}
               id="password"
               name="password"
               value={values.password}
@@ -143,7 +173,7 @@ export default function SignUp() {
                 errors.password ? "border-red-500" : "border-gray-300 "
               } mt-1 block w-full p-2 border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
             />
-            <ErrorMessage name="password" />
+            <ErrorMessage component={Error} name="password" />
           </div>
           <div className="mb-6 relative">
             {values.confirmPassword.length > 0 && (
@@ -155,7 +185,7 @@ export default function SignUp() {
 
             <Field
               label="Confirm Password"
-              type="confirmPassword"
+              type={isPasswordVisible ? "text" : "password"}
               id="confirmPassword"
               name="confirmPassword"
               value={values.confirmPassword}
@@ -167,7 +197,7 @@ export default function SignUp() {
                 errors.confirmPassword ? "border-red-500" : "border-gray-300 "
               } mt-1 block w-full p-2 border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm`}
             />
-            <ErrorMessage name="confirmPassword" />
+            <ErrorMessage component={Error} name="confirmPassword" />
           </div>
           <button
             type="submit"
